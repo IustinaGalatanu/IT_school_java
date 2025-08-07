88308e6c4133ac7512a23c8934e93229d2ff19f9
Iustina Galatanu <iustinagalatnu@gmail.com>
2025-08-07 18:41:58 +0300

    Homework-session 19/2

---
+++ src/main/java/org/example/Homework/session19/session19/Alph.java
@@ -0,0 +1,18 @@
+package org.example.Homework.session19.session19;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+//Write a program that uses lambda expressions to sort a list of strings in alphabetical order.
+public class Alph {
+    public static void main(String[] args) {
+
+        List<String> nume= Arrays.asList("Ana", "George","Bianca", "Andrei");
+        Comparator<String> alph=(n1,n2)->n1.compareTo(n2);
+        nume.sort(alph);
+
+        System.out.println("Numele in ordine alfabetica: "+nume);
+
+    }
+}
